592db1e598f9a3ba3e3af22cf63fac0c867e4608
BigRedFruitTree <blaine.dooley07@gmail.com>
2025-01-13 12:30:24 -0800

    The Low Taper Fade-update

---
+++ Test/Assets/Scripts/LinkedList.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkedList
+{
+    public Node head;
+    public int weapon;
+    
+    public void Switch()
+    {
+        
+    }
+}
